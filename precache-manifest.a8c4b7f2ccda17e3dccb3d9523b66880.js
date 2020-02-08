2e11bb27e2a9dfe97aa40dd3f067db67a2968ad1
Лев Цыпанов <51300421+levtsypanov@users.noreply.github.com>
2020-02-08 17:39:05 +0300

    Updates

---
+++ precache-manifest.a8c4b7f2ccda17e3dccb3d9523b66880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f9026db25152964052e",
+    "url": "/vk-apps-qr/static/js/runtime~main.d07b00a2.js"
+  },
+  {
+    "revision": "1d00dfc1a5281698b3ea",
+    "url": "/vk-apps-qr/static/js/main.4e43789e.chunk.js"
+  },
+  {
+    "revision": "71a3672ac09fb9364b55",
+    "url": "/vk-apps-qr/static/js/2.cc21c0ad.chunk.js"
+  },
+  {
+    "revision": "1d00dfc1a5281698b3ea",
+    "url": "/vk-apps-qr/static/css/main.17fc59d8.chunk.css"
+  },
+  {
+    "revision": "71a3672ac09fb9364b55",
+    "url": "/vk-apps-qr/static/css/2.15411077.chunk.css"
+  },
+  {
+    "revision": "274e3f310771e93103c26e505413e8f5",
+    "url": "/vk-apps-qr/index.html"
+  }
+];
